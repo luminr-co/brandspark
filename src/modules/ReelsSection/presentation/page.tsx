c7cfff9aa798889579dcb44c75179e4af40e6279
Rojen Maharjan <44132059+rojen11@users.noreply.github.com>
2024-03-31 13:53:51 +0545

    feat: added data and favicon

--- src/modules/ReelsSection/presentation/page.tsx
+++ src/modules/ReelsSection/presentation/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ReelsList } from "./components/ReelsList";
-import { ReelEntity } from "@/core/domain/entities/ReelEntity";
 
 export default function ReelsSection() {
   const reels = [
@@ -9,7 +8,10 @@ export default function ReelsSection() {
     "https://www.dropbox.com/scl/fi/vs2v9c5jaihcruunmq6sv/First-Time-Home-Buyer-Characteristic.mp4?rlkey=no2om2gzeinlf67t6150l6gbb&dl=0&raw=1",
     "https://www.dropbox.com/scl/fi/ckqm0fc1xpdue9in08ako/Eric-Brewer.mp4?rlkey=o2mkyc2ff1n1rxlkj5h9c28vx&dl=0&raw=1",
     "https://www.dropbox.com/scl/fi/kdkkhpqdgaq2zdd8u27qz/Aaron-Rodgers.mp4?rlkey=weayq57ppu7zzpknm1fdj94s0&dl=0&raw=1",
-    "https://www.dropbox.com/scl/fi/a4cfisijwzvv4fno3u6pe/Sample-1.mp4?rlkey=8e8xd2b8l6k2ucp687a0kpaaq&dl=0&raw=1"
+    "https://www.dropbox.com/scl/fi/a4cfisijwzvv4fno3u6pe/Sample-1.mp4?rlkey=8e8xd2b8l6k2ucp687a0kpaaq&dl=0&raw=1",
+    "https://www.dropbox.com/scl/fi/dumbphw8ae26tlnnkzmk0/Emmaus-Edited.mp4?rlkey=j8t8u3eti9q56shxrt0xrdex6&dl=0&raw=1",
+    "https://www.dropbox.com/scl/fi/1m1r5fw41xkvutllk2rkt/Tom-Ferry-Edited.mp4?rlkey=fag8wxqvte7u6qep76xcbu3ri&dl=0&raw=1",
+    "https://www.dropbox.com/scl/fi/nt58zn68mcbig7a8n8dm8/Top-3-things-that-hsbo-do-on-facebook.mp4?rlkey=hly0dx3iksaguwqbws3jgj4v1&dl=0&raw=1"
   ];
 
   return (
